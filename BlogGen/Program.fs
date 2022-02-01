9c0ad7b7553c81cf3ec6ff1c7d9be5f42821d3d8
WhiteBlackGoose <wbg@angouri.org>
2022-02-01 16:34:18 +0300

    All articles now there

--- BlogGen/Program.fs
+++ BlogGen/Program.fs
@@ -12,20 +12,87 @@ type Article = {
 }
 
 let articles = [
-    { lang = EN; tags = [ "C#" ];       title = "This is how Variadic Arguments could work in C#";
+    { lang = EN; tags = [ "C#" ];         title = "This is how Variadic Arguments could work in C#";
         link = "https://whiteblackgoose.medium.com/this-is-how-variadic-arguments-could-work-in-c-e2034a9c241" }
     
-    { lang = RU; tags = [ "F#"; "C#" ]; title = "Как LINQ, только быстрый и без аллокаций";
+    { lang = RU; tags = [ "F#"; "C#" ];   title = "Как LINQ, только быстрый и без аллокаций";
         link = "https://habr.com/ru/post/648529";  }
     
-    { lang = EN; tags = [ "C#" ];       title = "Like Regular LINQ, but Faster and Without Allocations: Is It Possible?";
+    { lang = EN; tags = [ "C#" ];         title = "Like Regular LINQ, but Faster and Without Allocations: Is It Possible?";
         link = "https://whiteblackgoose.medium.com/3d4724632e2a" }
     
-    { lang = RU; tags = [ "C#"; "F#" ]; title = "Очень типобезопасно! Концепт продвинутой расширяемой системы единиц измерения с generic math для .NET";
+    { lang = RU; tags = [ "C#"; "F#" ];   title = "Очень типобезопасно! Концепт продвинутой расширяемой системы единиц измерения с generic math для .NET";
         link = "https://habr.com/ru/post/597437/" }
     
-    { lang = EN; tags = [ "C#"; "F#" ]; title = "Stay safe with your units! Advanced units of measure in .NET.";
+    { lang = EN; tags = [ "C#"; "F#" ];   title = "Stay safe with your units! Advanced units of measure in .NET.";
         link = "https://whiteblackgoose.medium.com/stay-safe-with-your-units-advanced-units-of-measure-in-net-f7d8b02af87e" }
+    
+    { lang = RU; tags = [ "C#"; "perf" ]; title = "Ускоряем цикл foreach до for";
+        link = "https://habr.com/ru/post/575664/" }
+    
+    { lang = EN; tags = [ "C#"; "perf" ]; title = "Making loop \"foreach\" as fast as \"for\"";
+        link = "https://habr.com/en/post/575916/" }
+    
+    { lang = RU; tags = [ "C#"; "wasm" ]; title = "Хостим WASM-приложения на github pages в два клика";
+        link = "https://habr.com/ru/post/566286/" }
+    
+    { lang = RU; tags = [ "C#" ];         title = "Компилируем математические выражения";
+        link = "https://habr.com/ru/post/546622/" }
+    
+    { lang = EN; tags = [ "C#"; "F#"; "AngouriMath" ]; title = "AngouriMath 1.3 update";
+        link = "https://habr.com/en/post/565996/" }
+    
+    { lang = EN; tags = [ "C#" ];         title = "Compilation of symbolic expressions into Linq.Expression";
+        link = "https://habr.com/en/post/546926/" }
+    
+    { lang = EN; tags = [ "C#"; "oop" ];  title = "Lazy Properties Are Good. That Is How You Are to Use Them";
+        link = "https://habr.com/en/post/545936/" }
+    
+    { lang = EN; tags = [ "Jupyter"; ".NET" ]; title = "Math in Jupyter for .NET";
+        link = "https://www.reddit.com/r/dotnet/comments/jv7qqt/net_jupyter_math_looks_awesome/" }
+    
+    { lang = EN; tags = [ "F#"; "AngouriMath" ]; title = "https://am.angouri.org/research";
+        link = "AngouriMath for Research with F#" }
+    
+    { lang = EN; tags = [ "C#" ]; title = "Parsing a math expression in C#";
+        link = "https://whiteblackgoose.medium.com/parsing-a-math-expression-from-string-in-c-b2b48e2ac2e6" }
+    
+    { lang = EN; tags = [ ".NET"; "Jupyter" ]; title = "Jupyter in .NET";
+        link = "https://habr.com/en/post/528816" }
+    
+    { lang = RU; tags = [ "C#" ]; title = "Методы без аргументов — зло в ООП, и вот как его полечить";
+        link = "https://habr.com/ru/post/529454/" }
+    
+    { lang = RU; tags = [ ".NET"; "Jupyter" ]; title = "Jupyter для .NET. «Как в питоне»";
+        link = "https://habr.com/ru/post/528730/" }
+
+    { lang = RU; tags = [ "C#"; "F#" ]; title = "Что нового в AngouriMath 1.2?";
+        link = "https://habr.com/en/post/545190/" }
+
+    { lang = EN; tags = [ "C#"; "F#" ]; title = "What's new in AngouriMath 1.2?";
+        link = "https://habr.com/en/post/545436" }
+    
+    { lang = RU; tags = [ "C#"; "perf" ]; title = "Тензоры для C#. И матрицы, и векторы, и кастомный тип, и сравнительно быстро";
+        link = "https://habr.com/ru/post/512856/" }
+    
+    { lang = EN; tags = [ "C#"; "perf" ]; title = "Generic tensors in C#";
+        link = "https://gist.github.com/WhiteBlackGoose/5b84b2237704a91ffe7f34372196df32" }
+    
+    { lang = EN; tags = [ "python" ]; title = "Simple simulation of custom physical interactions with particles";
+        link = "https://dzone.com/articles/a-simple-simulation-of-custom-physical-interaction" }
+    
+    { lang = RU; tags = [ "python" ]; title = "Элементарная симуляция кастомного физического взаимодействия на python + matplotlib";
+        link = "https://habr.com/ru/post/467803/" }
+    
+    { lang = EN; tags = [ "python" ]; title = "Yet another snake with Kivy, Python";
+        link = "https://habr.com/en/post/465523" }
+    
+    { lang = RU; tags = [ "python" ]; title = "Играемся с комплексными числами";
+        link = "https://habr.com/ru/post/468781/" }
+    
+    { lang = RU; tags = [ "python" ]; title = "Генератор простых арифметических примеров для чайников и не только";
+        link = "https://habr.com/ru/post/468457/" }
+    
 ]
 
 let auto src = a [] $"https://{src}" src
