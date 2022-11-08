ce215d6ae9575688f6e4978f31f308a1e85f0836
WhiteBlackGoose <wbg@angouri.org>
2022-11-08 03:31:08 +0000

    Deploying to gh-pages from @ WhiteBlackGoose/WhiteBlackGoose@e1c745150409792dfb586020e948bff419d2303d 🚀

--- BlogGen/View.fs
+++ BlogGen/View.fs
@@ -16,6 +16,7 @@ let commonHead font =
         link [_rel "icon"; _type "image/png"; _href "https://avatars.githubusercontent.com/u/31178401"]
         link [_rel "stylesheet"; _type "text/css"; _href "https://fonts.googleapis.com/css?family=Overpass+Mono"]
         link [_rel "stylesheet"; _type "text/css"; _href "https://fonts.googleapis.com/css?family=Ubuntu+Mono"]
+        script [_src "https://buttons.github.io/buttons.js"] []
         makeStyle [] [
             lightTheme [
                 cssFilter "body" [
