e504af30f447557a464e62c972b68bdf0ff8eb23
WhiteBlackGoose <wbg@angouri.org>
2022-11-08 05:36:43 +0000

    Deploying to gh-pages from @ WhiteBlackGoose/WhiteBlackGoose@3f321e38578696a209fbe576e0cf58e01b63409b 🚀

--- BlogGen/Program.fs
+++ BlogGen/Program.fs
@@ -6,22 +6,29 @@ open Pages
 
 Directory.CreateDirectory "blog" |> ignore
 Directory.CreateDirectory "projects" |> ignore
+Directory.CreateDirectory "all" |> ignore
 
 let contents =
    let cs : Content list = (articles |> List.map (fun a -> Content.Article a)) @ (projects |> List.map (fun b -> Content.Project b))
-   cs |> List.sortBy dateOfContent
+   cs
+   |> List.sortBy dateOfContent
+   |> List.rev
+
+let genPage rootPath page name =
+    let path = Path.Combine(rootPath, name, "index.html")
+    let html = View.genPage contents page |> RenderView.AsString.htmlNode
+    printfn $"Writing to {Path.GetFullPath(path)}"
+    Directory.CreateDirectory (Path.GetDirectoryName path) |> ignore
+    printfn $"Length: {html.Length}"
+    File.WriteAllText(path, html)
+    printfn $"Ok.\n"
 
 let burnInto rootPath (pages : PageInfo list) =
     for { page = page; name = name } in pages do
-        let path = Path.Combine(rootPath, name, "index.html")
-        let html = View.genPage contents page |> RenderView.AsString.htmlNode
-        printfn $"Writing to {Path.GetFullPath(path)}"
-        Directory.CreateDirectory (Path.GetDirectoryName path) |> ignore
-        printfn $"Length: {html.Length}"
-        File.WriteAllText(path, html)
-        printfn $"Ok.\n"
+        genPage rootPath page name
 
 burnInto "./blog" articlePages
 burnInto "./projects" projectPages
+genPage "./all" allPage.page allPage.name
 
 printfn $"Finished."
