c7f776b1ac79161690078707f8424e30253d55ef
WhiteBlackGoose <wbg@angouri.org>
2023-01-23 19:56:17 +0300

    finished

--- BlogGen/InternalArticleView.fs
+++
@@ -1,4 +0,0 @@
-module InternalArticles.View
-
-let view content =
-    
--- BlogGen/View.fs
+++ BlogGen/View.fs
@@ -194,8 +194,9 @@ let genPage contents = function
             title [] [ Text pageTitle ]
             commonHead "sans-seriff"
             body [] [
-                let comp f = f [_style "padding: 12px;"] [
-                    Text $"""Blog of WhiteBlackGoose | This website is {_a "https://github.com/WhiteBlackGoose/WhiteBlackGoose/tree/master/BlogGen" "free software"} (GPLv3) | The {_a "https://github.com/WhiteBlackGoose/WhiteBlackGoose/tree/master/BlogGen/Contents/InternalArticles" "content"} is under CC BY-NC 4.0"""
+                let sp = span [_style "color: gray;"] [ Text "|" ] |> RenderView.AsString.htmlNode
+                let comp f = f [_style "padding: 12px; color: gray;"] [
+                    Text $"""{_a "/blog/index.html" "🏠 Home"} {sp} Blog of WhiteBlackGoose {sp} This website is {_a "https://github.com/WhiteBlackGoose/WhiteBlackGoose/tree/master/BlogGen" "free software"} (GPLv3) {sp} The {_a "https://github.com/WhiteBlackGoose/WhiteBlackGoose/tree/master/BlogGen/Contents/InternalArticles" "content"} is under CC BY-NC 4.0"""
                 ]
                 comp header
                 div [_class "article-body"] [
