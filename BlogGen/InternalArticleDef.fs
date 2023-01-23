ab237c6e86c0eba976099f505bc919ae7264682c
WhiteBlackGoose <wbg@angouri.org>
2023-01-23 17:21:50 +0000

    Deploying to gh-pages from @ WhiteBlackGoose/WhiteBlackGoose@ad893de8fedc9cff2e36765fd005b1d6b25c1218 🚀

--- BlogGen/InternalArticleDef.fs
+++ BlogGen/InternalArticleDef.fs
@@ -6,3 +6,14 @@ type TextPage = {
     title : string
     contents : XmlNode list
 }
+
+// thought of making ** and __ preprocessor
+// let md = 
+//     let bold = System.Text.RegularExpressions.Regex(@"\*\*(.*?)\*\*")
+//     let italics = System.Text.RegularExpressions.Regex(@"__(.*?)__")
+//     fun text ->
+//         text
+//         |> bold.Repla
+
+let bo text = b [] [ Text text ] |> RenderView.AsString.htmlNode
+let it text = i [] [ Text text ] |> RenderView.AsString.htmlNode
