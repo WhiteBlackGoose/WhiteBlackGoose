5680d4c5438383dc83278d0b68f1d1b1b68110a6
WhiteBlackGoose <wbg@angouri.org>
2023-01-24 16:41:14 +0300

    almost there

--- BlogGen/InternalArticleDef.fs
+++ BlogGen/InternalArticleDef.fs
@@ -28,6 +28,6 @@ let it text = i [] [ Text text ] |> RenderView.AsString.htmlNode
 let ur url name = a [_href url] [ Text name ] |> Giraffe.ViewEngine.RenderView.AsString.htmlNode
 let co text = span [_style "background: gray; font-family: mono;"] [ Text text ] |> Giraffe.ViewEngine.RenderView.AsString.htmlNode
 
-let anc tag = a [_href $"#{tag}"; _name tag] [ Text ">" ]
+let anc tag = a [_href $"#{tag}"; _name tag; _style "fill: gray;"] [ Text Css.iconLink ]
 let refanc tag text = a [_href $"#{tag}"] [ Text text ]
 
