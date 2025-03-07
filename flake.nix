83e4d924ca84d788ab9ba1dda8add5bd66bfd1e3
WhiteBlackGoose <wbg@angouri.org>
2025-03-07 17:55:41 +0100

    moved to .net 9

--- flake.nix
+++ flake.nix
@@ -6,11 +6,25 @@
     let 
       systems = [ "aarch64-linux" "x86_64-linux" ]; in {
     devShells = nixpkgs.lib.genAttrs systems (sys: { 
-      default = my-nix.dotnetShell 
+      default =
+      my-nix.dotnetShell
           nixpkgs.legacyPackages.${sys}
-          (p: [ p.sdk_7_0 ])
-          (p: [ p.fsautocomplete ])
-        ; 
+          (p: [ p.sdk_9_0 ])
+          (p: [
+            rec { 
+              binName = "fsautocomplete";
+              nugetName = binName;
+              dllName = binName; 
+              version = "0.77.4"; arch = "net9.0/any";
+              sha256 = "sha256-R9okn2dHioLPK49EHV43FsmdgZD8ijJzgZOonSmQAdw=";
+            }
+            rec {
+              binName = "dotnet-repl";
+              nugetName = binName;
+              dllName = binName;
+              version = "0.3.230"; arch = "net9.0/any";
+              sha256 = "sha256-I+LGFyiqmFxBIqQ6PhC9ZIEsU+yEtV4WFrm0nrI8biA=";
+            } ]);
       });
   };
 }
